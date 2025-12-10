a57c488f30b7300d51448a2a4f658d0be8ed7b67
Paul000n3s <paulooones@gmail.com>
2025-12-10 18:58:37 +0000

    Alterando a mensagem

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-  return <h1>Hello world!</h1>;
+  return (
+    <h1>
+      Qual a sua mensagem para a humanidade ET bilu: "Apenas que...busquem
+      conhecimento!"
+    </h1>
+  );
 }
 
 export default Home;
